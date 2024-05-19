f7d2892c072d5f9ce71abe31f99bcade3b55d1a1
Ali Naseri <mo.ali.naseri@gmail.com>
2024-05-19 13:30:12 +0330

    feat: setup simple component using storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: ['../app/**/*.mdx', '../app/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../libs/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-onboarding',
     '@storybook/addon-links',
